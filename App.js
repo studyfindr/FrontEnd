160f47de569db17748d9ba06a330ef0541996618
Will <wil1234@live.com.au>
2022-02-25 19:16:53 +1030

    Setup basic git cards

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import Card from './src/components/Card'
+
+const girl = {
+  image: '../../../assets/Girl.jpg',
+  name: 'Female Student',
+  bio: 'Second Year Computer Science Student Studying at Adelaide University'
+}
+
+const boy = {
+  image: 'Boy',
+  name: 'Male Student',
+  bio: 'First Year Environmental Science Student Studying at Adelaide University'
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.pageContainer}>
+      <Card user={boy}/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  pageContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
