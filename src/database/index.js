d477eb14158736e5c148cb37f314b13385636943
DhyanDarji <52266498+DhyanDarji@users.noreply.github.com>
2022-02-25 23:51:52 +1030

    pushing p and adding data

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,403 @@
 
 const Users = () => {
     return [
+  
         {
             id: 1,
-            name: "Will",
+            fullName: "Will Davis",
+            subjects: ["MATHS 1002", "PRGM 1001", "PSYCH 1001", "ENGL 2013"],
+            age: 19,
+            degree:"Computer Science",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 2,
+            fullName: "Jim Halpert",
+            subjects: ["COMP 1102","PHYS 2901", "PSYCH 1001", "LIT 1402"],
+            age: 19,
+            degree:"Maths",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 3,
+            fullName: "Cynthia Rose",
+            subjects: ["MATHS 1002", "LIT 1402", "ARTS 3029", "PSYCH 1001"],
+            age: 25,
+            degree:"Art",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+        {
+            id: 4,
+            fullName: "Abed Nadir",
+            subjects: ["LIT 1402", "MATHS 1002", "PSYCH 1001", "ENTREP 3901"],
+            age: 19,
+            degree:"Maths",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 5,
+            fullName: "Kallan Duong",
+            subjects: ["LIT 1402", "PSYCH 1001", "ENTREP 3901", "ENGL 2013"],
+            age: 21,
+            degree:"Computer Science",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 6,
+            fullName: "Troy Barnes",
+            subjects: ["ARTS 3029", "PRGM 1001", "PSYCH 1001", "MATHS 1002"],
+            age: 20,
+            degree:"English",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 7,
+            fullName: "Sheldon Cooper",
+            subjects: ["MATHS 1002", "PSYCH 1001", "LIT 1402", "DEN 2410"],
+            age: 19,
+            degree:"Physics",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 8,
+            fullName: "Ryan Howard",
+            subjects: ["ARTS 3029", "PHYS 2901", "DEN 2410", "MATHS 1002"],
+            age: 22,
+            degree:"Business",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 9,
+            fullName: "Rebecca Kendrick",
+            subjects: ["ARTS 3029", "PSYCH 1001", "LIT 1402", "ENGL 2013"],
+            age: 24,
+            degree:"Art",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 10,
+            fullName: "Amy Fowler",
+            subjects: ["COMP 1102", "PRGM 1001", "PSYCH 1001", "DEN 2410"],
+            age: 23,
+            degree:"Computer Science",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 11,
+            fullName: "Pam Beesly",
+            subjects: ["MATHS 1002", "ARTS 3029", "DEN 2410", "ENTREP 3901"],
+            age: 22,
+            degree:"Art",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 12,
+            fullName: "Benjamin Platt",
+            subjects: ["LIT 1402", "PHYS 2901", "PSYCH 1001", "MATHS 1002"],
+            age: 18,
+            degree:"Psychology",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 13,
+            fullName: "Annie Edison",
+            subjects: ["ARTS 3029", "PSYCH 1001", "LIT 1402", "ENGL 2013"],
+            age: 19,
+            degree:"Art",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 14,
+            fullName: "Ren You",
+            subjects: ["COMP 1102","DEN 2410","ENTREP 3901","LIT 1402"],
+            age: 19,
+            degree:"Philosophy",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 15,
+            fullName: "Michael Scott",
+            subjects: ["MATHS 1002", "LIT 1402", "PSYCH 1001", "ARTS 3029"],
+            age: 19,
+            degree:"Business",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+        
+        {
+            id: 16,
+            fullName: "Donald Ambudkar",
+            subjects: ["PHYS 2901", "PRGM 1001", "DEN 2410", "LIT 1402"],
+            age: 31,
+            degree:"English",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 17,
+            fullName: "Leslie Winkle",
+            subjects: ["MATHS 1002", "ENTREP 3901", "LIT 1402", "ENGL 2013"],
+            age: 31,
+            degree:"English",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 18,
+            fullName: "Angela Martin",
+            subjects: ["COMP 1102", "PHYS 2901", "DEN 2410", "ENTREP 3901"],
+            age: 19,
+            degree:"Spanish",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 19,
+            fullName: "Dhyan Darji",
+            subjects: ["MATHS 1002", "PSYCH 1001", "ARTS 3029", "DEN 2410"],
+            age: 19,
+            degree:"Computer Science",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 20,
+            fullName: "Chloe Snow",
+            subjects: ["ARTS 3029", "LIT 1402", "DEN 2410", "ENGL 2013"],
+            age: 20,
+            degree:"Art",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 21,
+            fullName: "Jeff Winger",
+            subjects: ["COMP 1102","ENGL 2013","PSYCH 1001","PHYS 2901"],
+            age: 21,
+            degree:"Law",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 22,
+            fullName: "Hattie Nguyen",
+            subjects: ["LIT 1402", "DEN 2410", "ARTS 3029", "ENTREP 3901"],
+            age: 20,
+            degree:"Computer Science",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 23,
+            fullName: "Britta Perry",
+            subjects: ["DEN 2410", "PHYS 2901", "ENTREP 3901", "ENGL 2013"],
+            age: 23,
+            degree:"Law",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 24,
+            fullName: "Irhas Gill",
+            subjects: ["ARTS 3029", "PHYS 2901", "PSYCH 1001", "ENGL 2013"],
+            age: 19,
+            degree:"Computer Science",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
+        },
+
+        {
+            id: 25,
+            fullName: "Raj Koothrappali",
+            subjects: ["COMP 1102", "PRGM 1001", "PHYS 2901", "LIT 1402"],
+            age: 20,
+            degree:"Computer Science",
+            swipedOn: {
+                2:false,
+                3:true
+               }
+
         }
+
     ]
 }
 
-export default Users;
+const Groups = () => {
+    return [
+        {
+            id: 1,
+            groupName: "Community",
+            members: [ 4, 6, 13, 21, 23 ],
+            swipedOn: {
+                1:true,
+                5:true,
+                3:true,
+                8:true,
+                15:true,
+            }
+        },
+
+        {
+            id: 2,
+            groupName: "The Office",
+            members: [ 2, 8, 11, 15, 18 ],
+            swipedOn: {
+                2:true,
+                6:true,
+                4:true,
+                9:true,
+                11:true,
+            }
+        },
+
+        {
+            id: 3,
+            groupName: "Big Bang Theory",
+            members: [ 7, 10, 17, 25 ],
+            swipedOn: {
+                7:true,
+                19:true,
+                20:true,
+                25:true,
+                13:true,
+            }
+        },
+
+        {
+            id: 4,
+            groupName: "Pitch Perfect",
+            members: [ 3, 9, 12, 16, 20 ],
+            swipedOn: {
+                24:true,
+                23:true,
+                12:true,
+                10:true,
+                16:true,
+            }
+        },
+
+        {
+            id: 5,
+            groupName: "Sudo Apt Get Coffee",
+            members: [ 1, 5, 14, 19, 22, 24 ],
+            swipedOn: {
+                14:true,
+                17:true,
+                18:true,
+                21:true,
+                22:true,
+            }
+        },
+
+    ]
+}
+
+
+export default {Users, Groups};
